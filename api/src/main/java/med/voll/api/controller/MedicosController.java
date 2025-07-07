d67390d25fb1ae1bb7c94a162b7b15541e93b4c3
agustin <agustinalvarez7378@gmail.com>
2025-07-07 09:01:33 -0400

    starting project

---
+++ api/src/main/java/med/voll/api/controller/MedicosController.java
@@ -0,0 +1,52 @@
+package med.voll.api.controller;
+
+
+import jakarta.validation.Valid;
+import med.voll.api.medico.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/medicos")
+public class MedicosController {
+
+    @Autowired
+    MedicoRepository repository;
+
+    @Transactional
+    @PostMapping
+    public void registrar(@RequestBody @Valid DatosRegistroMedico datos){
+        repository.save(new Medico(datos));
+
+    }
+    @GetMapping
+    public Page<DatosListaMedicos> listar(@PageableDefault(size=10, sort={"nombre"}) Pageable paginacion){
+        return repository.findAllByActivoTrue(paginacion).map(DatosListaMedicos::new);
+    }
+
+    @Transactional
+    @PutMapping
+    public void actualizar(@RequestBody @Valid DatosActualizacionMedico datos){
+        var medico = repository.getReferenceById(datos.id());
+        medico.actualizarInformaciones(datos);
+    }
+
+    // para hacer una eliminacion fisica del medico
+//    @Transactional
+//    @DeleteMapping("/{id}")
+//    public void eliminar(@PathVariable Long id){
+//        repository.deleteById(id);
+//    }
+
+    //para hacer eliminacion logica del medico
+    @Transactional
+    @DeleteMapping("/{id}")
+    public void eliminar(@PathVariable Long id){
+        var medico = repository.getReferenceById(id);
+        medico.elimiar();
+    }
+}
