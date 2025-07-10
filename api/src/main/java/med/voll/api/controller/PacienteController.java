6a1ad63f1b299e0036d97425da5d3161aa5da774
agustin <agustinalvarez7378@gmail.com>
2025-07-10 12:13:20 -0400

    adding version v4 and v5 to migrations flaway versions

--- api/src/main/java/med/voll/api/controller/PacienteController.java
+++ api/src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,9 +1,6 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
-import med.voll.api.medico.DatosDetallesMedico;
-import med.voll.api.medico.DatosListaMedicos;
-import med.voll.api.medico.Medico;
 import med.voll.api.paciente.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
