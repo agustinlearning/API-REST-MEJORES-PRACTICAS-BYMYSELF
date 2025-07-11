36c9ebd1cf7c84202a5394ed43cada04761d4411
agustin <agustinalvarez7378@gmail.com>
2025-07-11 12:16:25 -0400

    creating dto DatosTokenJWT for return a DTO

--- api/src/main/java/med/voll/api/controller/AutenticacionController.java
+++ api/src/main/java/med/voll/api/controller/AutenticacionController.java
@@ -3,6 +3,7 @@ package med.voll.api.controller;
 import jakarta.validation.Valid;
 import med.voll.api.domain.usuario.DatosAutenticacion;
 import med.voll.api.domain.usuario.Usuario;
+import med.voll.api.infra.security.DatosTokenJWT;
 import med.voll.api.infra.security.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -25,9 +26,11 @@ public class AutenticacionController {
 
     @PostMapping
     public ResponseEntity iniciarSesion(@RequestBody @Valid DatosAutenticacion datos){
-        var token = new UsernamePasswordAuthenticationToken(datos.login(), datos.contrasena());
-        var autentiacacion = manager.authenticate(token);
+        var authenticationToken = new UsernamePasswordAuthenticationToken(datos.login(), datos.contrasena());
+        var autentiacacion = manager.authenticate(authenticationToken);
 
-        return ResponseEntity.ok(tokenService.generarToken((Usuario) autentiacacion.getPrincipal()));
+        var tokenJWT = tokenService.generarToken((Usuario) autentiacacion.getPrincipal());
+
+        return ResponseEntity.ok(new DatosTokenJWT(tokenJWT));
     }
 }
