7d1f775dad705acd8cf95f246ec62b6afbe6b313
agustin <agustinalvarez7378@gmail.com>
2025-07-11 09:02:14 -0400

    Creating method loadUerByUsername and injecting dependency UsuarioRepository to AutenticacionService and creating method findByLogin in UsuarioRepository interface

--- api/src/main/java/med/voll/api/domain/usuario/AutenticacionService.java
+++ api/src/main/java/med/voll/api/domain/usuario/AutenticacionService.java
@@ -1,5 +1,6 @@
 package med.voll.api.domain.usuario;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -7,8 +8,11 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class AutenticacionService implements UserDetailsService {
+    @Autowired
+    UsuarioRepository repository;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        return repository.findByLogin(username);
     }
 }
